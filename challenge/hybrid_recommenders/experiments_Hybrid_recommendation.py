7d4dc0d126804b33b98a7bc298280de677fde4b7
Jody Roberto Battistini <jodi.battistini@libero.it>
2023-12-02 17:40:35 +0100

    feat(challenge): add frozen matrices

--- challenge/hybrid_recommenders/experiments_Hybrid_recommendation.py
+++ challenge/hybrid_recommenders/experiments_Hybrid_recommendation.py
@@ -16,11 +16,8 @@ def __main__():
     users_file_path = '../input_files/data_target_users_test.csv'
     URM_all_dataframe, users_list = read_data(data_file_path, users_file_path)
 
-    URM_all = sps.coo_matrix(
-        (URM_all_dataframe['Data'].values, (URM_all_dataframe['UserID'].values, URM_all_dataframe['ItemID'].values)))
-    URM_all = URM_all.tocsr()
-
-    URM_train, URM_test = split_train_in_two_percentage_global_sample(URM_all, train_percentage=0.80)
+    URM_train = sps.load_npz('../input_files/URM_train_plus_validation.npz')
+    URM_test = sps.load_npz('../input_files/URM_test.npz')
 
     evaluator = EvaluatorHoldout(URM_test, cutoff_list=cutoff_list)
 
@@ -29,20 +26,32 @@ def __main__():
                          feature_weighting="TF-IDF")
     item_Wsparse = item_recommender.W_sparse
 
+    results, _ = evaluator.evaluateRecommender(item_recommender)
+    print("MAP: {}".format(results.loc[10]["MAP"]))
+
     P3_recommender = P3alphaRecommender.P3alphaRecommender(URM_train)
     P3_recommender.fit(topK=64, alpha=0.35496275558011753, min_rating=0.1, implicit=True,
                        normalize_similarity=True)
     P3_Wsparse = P3_recommender.W_sparse
+
+    results, _ = evaluator.evaluateRecommender(P3_recommender)
+    print("MAP: {}".format(results.loc[10]["MAP"]))
 
     RP3_recommender = RP3betaRecommender.RP3betaRecommender(URM_train)
     RP3_recommender.fit(topK=30, alpha=0.26362900188025656, beta=0.17133265585189086, min_rating=0.2588031389774553,
                         implicit=True, normalize_similarity=True)
     RP3_Wsparse = RP3_recommender.W_sparse
 
+    results, _ = evaluator.evaluateRecommender(RP3_recommender)
+    print("MAP: {}".format(results.loc[10]["MAP"]))
+
     SLIM_recommender = SLIMElasticNetRecommender.SLIMElasticNetRecommender(URM_train)
     SLIM_recommender.fit(l1_ratio=0.005997129498003861, alpha=0.004503120402472539,
                          positive_only=True, topK=45)
     SLIM_Wsparse = SLIM_recommender.W_sparse
+
+    results, _ = evaluator.evaluateRecommender(SLIM_recommender)
+    print("MAP: {}".format(results.loc[10]["MAP"]))
 
     recommenders = [item_recommender, P3_recommender, RP3_recommender, SLIM_recommender]
     alphas = [0.5, 0.65, 0.75, 1.0]
@@ -59,7 +68,6 @@ def __main__():
     generate_submission_csv("../output_files/HybridSubmission.csv", recommendations)
 
     results, _ = evaluator.evaluateRecommender(recommender_object)
-
     print("MAP: {}".format(results.loc[10]["MAP"]))
 
 
